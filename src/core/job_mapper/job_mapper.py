7476a688512238dd8561a5ea8cd1117692292ed0
DereckTav <derecktaverne@gmail.com>
2026-01-14 20:06:12 -0500

    fleshed out skeleton for turning data into src.models.job

---
+++ src/core/job_mapper/job_mapper.py
@@ -0,0 +1,10 @@
+from typing import List
+from src.models.job import Job
+
+class JobMapper:
+    pass
+
+# should be smart about deciding how to map job
+# takes in df
+
+# can decide on which mapper to use
--- src/core/services/llm/job_service.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from src.models.job import Job
-
-class JobService:
-
-    def extract_job_from(self, url: str) -> List[Job]:
-        pass
